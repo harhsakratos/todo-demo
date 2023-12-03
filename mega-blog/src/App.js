a37d1f65558f652d2936b18725f1435ad0607ab3
harshakratos <sriharsha6madagiri@gmail.com>
2023-12-03 17:15:54 +0530

    initial setup and auth page add

---
+++ mega-blog/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  console.log(process.env.REACT_APP_APPWRITE_URL)
+  return (
+  <>
+  <h1> A Blog with appWirte</h1>
+  </>
+  );
+}
+
+export default App;
