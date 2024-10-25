8a53d1675de4f4611a4255b4826dba7157c358f9
harshakratos <sriharsha6madagiri@gmail.com>
2024-10-26 02:55:45 +0530

    Todo development till form and table

--- basic-react/context-practice/src/Context/MyContext.js
+++ basic-react/context-practice/src/Context/MyContext.js
@@ -3,16 +3,13 @@ import { createContext } from "react";
 
 export const MyContext = createContext();
 
-
-const StateProvider = ({children}) =>{
-    const [name,setName] = React.useState("harsha");
-    return (
-    <MyContext.Provider value={{name,setName}}>
-    {children}
+const StateProvider = ({ children }) => {
+  const [name, setName] = React.useState("harsha");
+  return (
+    <MyContext.Provider value={{ name, setName }}>
+      {children}
     </MyContext.Provider>
-    )
-}
-
-
+  );
+};
 
 export default StateProvider;
